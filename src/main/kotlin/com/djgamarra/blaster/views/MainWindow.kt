d85b4f5a3d0fe4ffd1c8e31abcaa0311c9c5f90d
Jose David Padilla Gamarra <jd.padillagamarra@gmail.com>
2024-09-30 14:28:17 -0500

    feat: implement canvas buffer strategy

--- src/main/kotlin/com/djgamarra/blaster/views/MainWindow.kt
+++ src/main/kotlin/com/djgamarra/blaster/views/MainWindow.kt
@@ -2,15 +2,16 @@ package com.djgamarra.blaster.views
 
 import com.djgamarra.blaster.workers.DrawerWorker
 import java.awt.Dimension
-import java.awt.Graphics2D
-import java.awt.Toolkit
+import java.awt.image.BufferStrategy
 import java.awt.image.BufferedImage
 import javax.swing.JFrame
 import javax.swing.SwingUtilities
 
+
 class MainWindow : JFrame() {
     private val worker = DrawerWorker(this)
     private val canvas = GameCanvas()
+    private val strategy: BufferStrategy
 
     init {
         defaultCloseOperation = EXIT_ON_CLOSE
@@ -22,22 +23,30 @@ class MainWindow : JFrame() {
         size = Dimension(ViewUtils.VIEWPORT_WIDTH, ViewUtils.VIEWPORT_HEIGHT)
         location = ViewUtils.defaultGraphicsEnvironment.centerPoint.apply {
             translate(
-                -ViewUtils.VIEWPORT_WIDTH / 2,
-                -ViewUtils.VIEWPORT_HEIGHT / 2
+                -ViewUtils.VIEWPORT_WIDTH / 2, -ViewUtils.VIEWPORT_HEIGHT / 2
             )
         }
 
         add(canvas)
+        pack()
+
+        canvas.createBufferStrategy(2)
+        strategy = canvas.bufferStrategy
 
         worker.start()
     }
 
     fun drawTick(image: BufferedImage) {
         SwingUtilities.invokeLater {
-            val graphics = canvas.graphics as Graphics2D
-            graphics.drawImage(image, 0, 0, null)
-            graphics.dispose()
-            Toolkit.getDefaultToolkit().sync()
+            do {
+                do {
+                    val graphics = strategy.drawGraphics
+                    graphics.drawImage(image, 0, 0, null)
+                    graphics.dispose()
+                } while (strategy.contentsRestored())
+
+                strategy.show()
+            } while (strategy.contentsLost())
         }
     }
 }
--- src/main/kotlin/com/djgamarra/blaster/views/ViewUtils.kt
+++ src/main/kotlin/com/djgamarra/blaster/views/ViewUtils.kt
@@ -9,7 +9,7 @@ import java.awt.image.BufferedImage
 object ViewUtils {
     const val VIEWPORT_HEIGHT = 700
     const val VIEWPORT_WIDTH = 900
-    const val MAX_FPS = 65L
+    const val MAX_FPS = 60L
 
     val GAME_BACKGROUND_COLOR = Color(33, 33, 33)
 
