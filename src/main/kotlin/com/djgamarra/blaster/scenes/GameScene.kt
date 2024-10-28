cc848908afaaa23dd5440e54f8a8e1ab4ce1ec2a
Jose David Padilla Gamarra <jd.padillagamarra@gmail.com>
2024-10-27 21:15:17 -0500

    feat: game over label ok

--- src/main/kotlin/com/djgamarra/blaster/scenes/GameScene.kt
+++ src/main/kotlin/com/djgamarra/blaster/scenes/GameScene.kt
@@ -1,9 +1,11 @@
 package com.djgamarra.blaster.scenes
 
 import com.djgamarra.blaster.data.RenderContext
+import com.djgamarra.blaster.scenes.components.Button
 import com.djgamarra.blaster.scenes.components.Opponent
 import com.djgamarra.blaster.scenes.components.OpponentsBlock
 import com.djgamarra.blaster.scenes.components.Player
+import com.djgamarra.blaster.utils.ViewUtils
 import java.awt.Graphics2D
 import java.awt.event.MouseEvent
 
@@ -11,9 +13,21 @@ class GameScene : Scene() {
     private val opponentsBlock = OpponentsBlock()
     private val player = Player()
 
+    private val exitButton =
+        Button("Exit", x = ViewUtils.spacing(1), y = ViewUtils.VIEWPORT_HEIGHT - 50 - ViewUtils.spacing(), onClick = {
+            RootScene.changeScene(StartScene())
+        })
+    private val gameOverLabel = Button("Game Over", fontSize = 50F, active = true, x = 0, y = 0)
+
     private var dead = false
 
+    override fun mouseClicked(e: MouseEvent) {
+        exitButton.mouseClicked(e)
+    }
+
     override fun mouseMoved(e: MouseEvent) {
+        exitButton.mouseMoved(e)
+
         if (dead) {
             return
         }
@@ -35,6 +49,11 @@ class GameScene : Scene() {
     override fun draw(g: Graphics2D, ctx: RenderContext) {
         player.draw(g, ctx)
         opponentsBlock.draw(g, ctx)
+        exitButton.draw(g, ctx)
+
+        if (dead) {
+            gameOverLabel.draw(g, ctx)
+        }
     }
 
     private fun checkCollisions() {
